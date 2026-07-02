dcae0acbde748c6f90064887c56bdb2230356d65
Morfin <morfinledon@gmail.com>
2026-07-02 15:18:30 -0700

    temp: restore completo CC1

--- cleanup.js
+++ cleanup.js
@@ -2,26 +2,26 @@ require('dotenv').config();
 const { query } = require('./db/connection');
 
 async function run() {
-  // Ver todos los campos de un reporte similar para inferir los valores
-  const ref = await query(`
-    SELECT TOP 1 CLAVE_REP, REPORTE, CLAVE_SERIE, CLAVE_GRUPO, CLAVE_REG,
-      CLAVE_SECCION_REP, CLAVE_VERSION_REPORTE, CLAVE_PERIODO,
-      DESCRIPCION_ESP, CLAVE_REGULACION_REP, CLAVE_REP_GENERAL
-    FROM INVENTARIO_REPORTES
-    WHERE CLAVE_PAIS='GT' AND CLAVE_ENTIDADREGULADA='BM' AND REPORTE IS NOT NULL
-      AND CLAVE_REP != 'GT_BM_CC1_CC1_00_22'
+  await query(`
+    UPDATE INVENTARIO_REPORTES SET
+      REPORTE               = 'CC1',
+      CLAVE_SERIE           = 'CC1',
+      CLAVE_GRUPO           = 'CARTERA',
+      CLAVE_REG             = 'SUPER',
+      CLAVE_SECCION_REP     = '00',
+      CLAVE_VERSION_REPORTE = '22',
+      DESCRIPCION_ESP       = 'CC1 INFORMACIÓN DE CRÉDITOS NUEVOS',
+      CLAVE_REP_GENERAL     = 'GT_BM_CC1_CC1_00'
+    WHERE CLAVE_REP = 'GT_BM_CC1_CC1_00_22'
   `);
-  console.log('Reporte de referencia:', JSON.stringify(ref, null, 2));
+  console.log('Restaurado completamente.');
 
-  // Ver también si hay otro reporte CC1 de otra versión para referencia exacta
-  const cc1ref = await query(`
-    SELECT TOP 3 CLAVE_REP, REPORTE, CLAVE_SERIE, CLAVE_GRUPO, CLAVE_REG,
-      CLAVE_SECCION_REP, CLAVE_VERSION_REPORTE, CLAVE_PERIODO,
-      DESCRIPCION_ESP, CLAVE_REGULACION_REP, CLAVE_REP_GENERAL
-    FROM INVENTARIO_REPORTES
-    WHERE CLAVE_REP LIKE '%CC1%'
+  const check = await query(`
+    SELECT REPORTE, CLAVE_SERIE, CLAVE_GRUPO, CLAVE_REG, CLAVE_PERIODO,
+           DESCRIPCION_ESP, CLAVE_REP_GENERAL, CLAVE_REGULACION_REP
+    FROM INVENTARIO_REPORTES WHERE CLAVE_REP='GT_BM_CC1_CC1_00_22'
   `);
-  console.log('Reportes CC1:', JSON.stringify(cc1ref, null, 2));
+  console.log(JSON.stringify(check, null, 2));
 }
 
 run().catch(e => console.error(e.message));
